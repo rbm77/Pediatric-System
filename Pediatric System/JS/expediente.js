b7f7d2e5348fc6b7e5160bf95dd0cead09212a71
Fabian Jimenez Morales <fabian.jimenez@ucrso.info>
2019-05-23 02:28:05 -0600

    Parte 2 y 3 - Ficha Base Expediente

---
+++ Pediatric System/JS/expediente.js
@@ -0,0 +1,46 @@
+﻿
+$(document).ready(function () {
+    $(".step2").hide();
+    $(".step3").hide();
+    $(".step4").hide();
+
+    $(".boxSave").hide();
+});
+
+function ocultarStep1() {
+    $(".step1").hide();
+    $(".step2").show();
+
+    $(".progress-bar").addClass("progress-bar-50");
+}
+
+function ocultarStep2() {
+    $(".step1").hide();
+    $(".step2").hide();
+    $(".step3").show();
+
+    $(".progress-bar").addClass("progress-bar-75");
+}
+
+function ocultarStep3() {
+    $(".step1").hide();
+    $(".step2").hide();
+    $(".step3").hide();
+    $(".step4").show();
+
+    $(".progress-bar").addClass("progress-bar-100");
+    $(".boxSave").show();
+}
+
+function mostrarStep1() {
+    $(".step1").show();
+    $(".step2").hide();
+    $(".step3").hide();
+    $(".step4").show();
+
+    $(".progress-bar").removeClass("progress-bar-100");
+    $(".progress-bar").removeClass("progress-bar-75");
+    $(".progress-bar").removeClass("progress-bar-50");
+
+    $(".boxSave").hide();
+}
