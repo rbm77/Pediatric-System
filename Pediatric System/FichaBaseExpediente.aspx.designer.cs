7f2ce66931f7a0c31394e78f29eab56eeb65393b
Fabian Jimenez Morales <fabian.jimenez@ucrso.info>
2019-05-23 21:34:48 -0600

    Ficha base de expediente lista

--- Pediatric System/FichaBaseExpediente.aspx.designer.cs
+++ Pediatric System/FichaBaseExpediente.aspx.designer.cs
@@ -13,15 +13,6 @@ namespace Pediatric_System {
     public partial class FichaBaseExpediente {
         
         /// <summary>
-        /// Image1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Image Image1;
-        
-        /// <summary>
         /// fuploadImagen control.
         /// </summary>
         /// <remarks>
