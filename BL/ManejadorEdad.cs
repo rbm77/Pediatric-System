e875a949a213ff78cba4f7389a87119e770be525
Richard Bolaños Moya <richardbomo26@gmail.com>
2019-07-01 00:29:45 -0600

    inicioVacunas

---
+++ BL/ManejadorEdad.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class ManejadorEdad
+    {
+
+        public ManejadorEdad()
+        {
+
+        }
+
+        /// <summary>
+        /// Obtiene la edad de un paciente a partir de la fecha de nacimiento
+        /// </summary>
+        /// <param name="fechaNacimiento"></param>
+        /// <returns>Retorna una descripcion de la edad</returns>
+        public string ExtraerEdad(DateTime fechaNacimiento)
+        {
+
+            TimeSpan d = DateTime.Now - fechaNacimiento;
+
+            int dias = d.Days;
+
+            int meses = 0;
+            int annos = 0;
+
+            if (dias < 30)
+            {
+                if (dias == 1)
+                {
+                    return dias + "día";
+                }
+                return dias + " días";
+            }
+
+            if (dias >= 30)
+            {
+                meses = dias / 30;
+
+                if (meses >= 12)
+                {
+                    annos = meses / 12;
+
+                    int sobranteMeses = meses - (annos * 12);
+
+                    if (sobranteMeses == 0)
+                    {
+                        if (annos == 1)
+                        {
+                            return annos + "año";
+                        }
+                        return annos + " años";
+                    }
+
+                    if(annos == 1 && sobranteMeses == 1)
+                    {
+                        return annos + " año y " + sobranteMeses + " mes";
+                    }
+                    if (annos == 1 && sobranteMeses > 1)
+                    {
+                        return annos + " año y " + sobranteMeses + " meses";
+                    }
+                    if (annos > 1 && sobranteMeses == 1)
+                    {
+                        return annos + " años y " + sobranteMeses + " mes";
+                    }
+                    return annos + " años y " + sobranteMeses + " meses";
+                }
+                else
+                {
+                    if (meses == 1)
+                    {
+                        return meses + " mes";
+                    }
+
+                    return meses + " meses";
+                }
+            }
+            return "error";
+        }
+
+        /// <summary>
+        /// Extrae la cantidad de meses que tiene el paciente a partir de la fecha de naciemiento
+        /// </summary>
+        /// <param name="fechaNacimiento"></param>
+        /// <returns>Retorna la cantidad de meses</returns>
+        public int ExtraerMeses(DateTime fechaNacimiento)
+        {
+            TimeSpan d = DateTime.Now - fechaNacimiento;
+
+            int dias = d.Days;
+
+            return dias / 30;
+        }
+
+    }
+}
---
+++ DAO/DAOVacuna.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TO;
+
+namespace DAO
+{
+    public class DAOVacuna
+    {
+        //Se establece la propiedad de conexion con la base de datos
+        SqlConnection conexion = new SqlConnection(Properties.Settings.Default.conexion);
+
+        public string CargarEsquemaVacunacion(List<TOVacuna> vacunas, List<TOVacuna> aplicaciones, string idExpediente)
+        {
+            string confirmacion = "El esquema de vacunación se cargó exitosamente";
+
+            // Se abre la conexión
+
+            if (conexion != null)
+            {
+                try
+                {
+                    if (conexion.State != ConnectionState.Open)
+                    {
+                        conexion.Open();
+                    }
+                }
+                catch (Exception)
+                {
+                    confirmacion = "Ocurrió un error y no se pudo cargar el esquema de vacunación";
+                    return confirmacion;
+                }
+            }
+            else
+            {
+                confirmacion = "Ocurrió un error y no se pudo cargar el esquema de vacunación";
+                return confirmacion;
+            }
+
+            // Se inicia una nueva transacción
+
+            SqlTransaction transaccion = conexion.BeginTransaction("Cargar esquema de vacunación");
+
+
+
+            try
+            {
+
+                // Se crea un nuevo comando con la secuencia SQL y el objeto de conexión
+
+                SqlCommand comando = new SqlCommand("SELECT * FROM VACUNA;", conexion);
+
+
+                comando.Transaction = transaccion;
+
+                // Se ejecuta el comando 
+
+                SqlDataReader lector = comando.ExecuteReader();
+
+                // Se lee el dataReader con los registros obtenidos y se cargan los datos en la lista de vacunas
+
+                if (lector.HasRows)
+                {
+                    while (lector.Read())
+                    {
+                        TOVacuna vacuna = new TOVacuna(lector["NOMBRE_VACUNA"].ToString(), lector["APLICACION1"].ToString(),
+                            lector["APLICACION2"].ToString(), lector["APLICACION3"].ToString(),
+                            lector["REFUERZO1"].ToString(), lector["REFUERZO2"].ToString(),
+                            lector["REFUERZO3"].ToString());
+
+                        vacunas.Add(vacuna);
+
+                    }
+                }
+
+                lector.Close();
+
+                comando.CommandText = "SELECT * FROM APLICACION_VACUNA WHERE ID_EXPEDIENTE = @idExpediente";
+
+                comando.Parameters.AddWithValue("@idExpediente", idExpediente);
+
+                SqlDataReader lector2 = comando.ExecuteReader();
+
+                if (lector2.HasRows)
+                {
+                    while (lector2.Read())
+                    {
+                        TOVacuna aplicacion = new TOVacuna(lector2["NOMBRE_VACUNA"].ToString(), lector2["APLICACION1"].ToString(),
+                            lector2["APLICACION2"].ToString(), lector2["APLICACION3"].ToString(),
+                            lector2["REFUERZO1"].ToString(), lector2["REFUERZO2"].ToString(),
+                            lector2["REFUERZO3"].ToString());
+
+                        aplicaciones.Add(aplicacion);
+
+                    }
+                }
+                lector2.Close();
+
+                transaccion.Commit();
+
+            }
+            catch (Exception)
+            {
+                try
+                {
+
+                    // En caso de un error se realiza un rollback a la transacción
+
+                    transaccion.Rollback();
+                }
+                catch (Exception)
+                {
+                }
+                finally
+                {
+                    confirmacion = "Ocurrió un error y no se pudo cargar el esquema de vacunación";
+                }
+            }
+            finally
+            {
+                if (conexion.State != ConnectionState.Closed)
+                {
+                    conexion.Close();
+                }
+            }
+            return confirmacion;
+        }
+
+
+
+        //INSERT INTO APLICACION_VACUNA(ID_EXPEDIENTE, NOMBRE_VACUNA) VALUES('@ID', 'Antineumocóccica');
+        //INSERT INTO APLICACION_VACUNA(ID_EXPEDIENTE, NOMBRE_VACUNA) VALUES('@ID', 'Antipolio, inactivada, vía intramuscular (IPV)');
+        //INSERT INTO APLICACION_VACUNA(ID_EXPEDIENTE, NOMBRE_VACUNA) VALUES('@ID', 'Antisarampionosa, rubéola y paperas (SRP)');
+        //INSERT INTO APLICACION_VACUNA(ID_EXPEDIENTE, NOMBRE_VACUNA) VALUES('@ID', 'Antituberculosa (BCG)');
+        //INSERT INTO APLICACION_VACUNA(ID_EXPEDIENTE, NOMBRE_VACUNA) VALUES('@ID', 'CALOSTRO (primera vacuna)');
+        //INSERT INTO APLICACION_VACUNA(ID_EXPEDIENTE, NOMBRE_VACUNA) VALUES('@ID', 'Haemophilus influenzae. Tipo B.(HIB)');
+        //INSERT INTO APLICACION_VACUNA(ID_EXPEDIENTE, NOMBRE_VACUNA) VALUES('@ID', 'Hepatitis B.(VHB)');
+        //INSERT INTO APLICACION_VACUNA(ID_EXPEDIENTE, NOMBRE_VACUNA) VALUES('@ID', 'Toxoide diftérico, pertusis acelular (DTaP)');
+        //INSERT INTO APLICACION_VACUNA(ID_EXPEDIENTE, NOMBRE_VACUNA) VALUES('@ID', 'Varicela');
+
+    }
+}
