84779c121e18f6ea2dd7ad017901e4ed102a151f
LAPTOP-PQVQE3LE\aleli <jose.lizano@ucrso.info>
2019-07-12 18:34:30 -0600

    Update BLEnviarCorreo.cs

--- BL/BLEnviarCorreo.cs
+++ BL/BLEnviarCorreo.cs
@@ -30,8 +30,9 @@ namespace BL
             correo.Subject = asunto;
             correo.SubjectEncoding = System.Text.Encoding.UTF8;
 
+            mensaje = mensaje + "\n pediatricarobertmoya.com";
             ///Se asigna el mensaje del correo
-            correo.Body = mensaje;
+            correo.Body = mensaje 
             correo.BodyEncoding = System.Text.Encoding.UTF8;
             correo.IsBodyHtml = false;
 
